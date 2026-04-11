d468d93057a5a5e988e62d2fb4b145245c89a151
jesslynlaurent <jesslynlaurentt12@gmail.com>
2026-04-11 15:47:37 +0100

    waters script

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 
-public class Water : MonoBehaviour
+public class WaterScroll : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Renderer rend;
+    public float speedX = 0.03f;
+    public float speedY = 0.01f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 offset = new Vector2(Time.time * speedX, Time.time * speedY);
+
+        if (rend.material.HasProperty("_BaseMap"))
+            rend.material.SetTextureOffset("_BaseMap", offset);
+        else
+            rend.material.mainTextureOffset = offset;
     }
 }
