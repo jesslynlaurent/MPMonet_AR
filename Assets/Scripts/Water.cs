1280e9b4fa2b4c2f1f60e14b8ea2f0ea96ed70c5
jesslynlaurent <jesslynlaurentt12@gmail.com>
2026-04-18 17:29:26 +0100

    script

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 public class Water : MonoBehaviour
 {
     public Renderer rend;
-    public float speedX = 0.03f;
-    public float speedY = 0.01f;
+    public float speedX = 0.01f;
+    public float speedY = 0.005f;
+
+    private Vector2 offset;
+
+    void Start()
+    {
+        if (rend == null)
+        rend = GetComponent<Renderer>();
+    }
 
     void Update()
     {
-        Vector2 offset = new Vector2(Time.time * speedX, Time.time * speedY);
-
+        if (rend == null) return;
+        offset += new Vector2(speedX, speedY) * Time.deltaTime;
+        
         if (rend.material.HasProperty("_BaseMap"))
-            rend.material.SetTextureOffset("_BaseMap", offset);
+        rend.material.SetTextureOffset("_BaseMap",offset);
         else
-            rend.material.mainTextureOffset = offset;
+        rend.material.mainTextureOffset =  offset;
     }
 }
