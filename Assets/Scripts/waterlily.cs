308f113f63c61fb3c03dcd6cbe95eb9dca38da5e
jesslynlaurent <jesslynlaurentt12@gmail.com>
2026-04-18 17:54:29 +0100

    waterlily script

--- Assets/Scripts/waterlily.cs
+++ Assets/Scripts/waterlily.cs
@@ -2,27 +2,28 @@ using UnityEngine;
 
 public class waterlily : MonoBehaviour
 {
-
-    public float moveAmount = 0.002f;
-    public float moveSpeed = 0.8f;
+    public float moveAmount = 0.01f;
+    public float moveSpeed = 1f;
+    public float rotateAmount = 3f;
+    public float rotateSpeed = 0.8f;
 
     private Vector3 startPos;
+    private Quaternion startRot;
     private float randomOffset;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         startPos = transform.localPosition;
+        startRot = transform.localRotation;
         randomOffset = Random.Range(0f, 10f);
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-       float yMove = Mathf.Sin((Time.time + randomOffset) * moveSpeed) * moveAmount;
-       float zRot = Mathf.Sin((Time.time + randomOffset) * rotateSpeed) * rotateAmount;
+        float yMove = Mathf.Sin((Time.time + randomOffset) * moveSpeed) * moveAmount;
+        float zRot = Mathf.Sin((Time.time + randomOffset) * rotateSpeed) * rotateAmount;
 
-       transform.localPosition = startPos + new Vector3(0f, yMove, 0f);
-       transform.localRotation = startRot * Quaternion.Euler(0f, 0f, zRot);
+        transform.localPosition = startPos + new Vector3(0f, yMove, 0f);
+        transform.localRotation = startRot * Quaternion.Euler(0f, 0f, zRot);
     }
 }
