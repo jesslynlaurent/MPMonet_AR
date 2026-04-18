a30b06e0694e9d65c0e6d3e5c39998007a82d5b6
jesslynlaurent <jesslynlaurentt12@gmail.com>
2026-04-18 18:00:00 +0100

    tree script

--- Assets/Scripts/tree.cs
+++ Assets/Scripts/tree.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class tree : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float swayAmountX = 1f;
+    public float swayAmountZ = 1.5f;
+    public float swaySpeed = 0.5f;
+
+    private Quaternion startRot;
+    private float randomOffset;
+
     void Start()
     {
-        
+        startRot = transform.localRotation;
+        randomOffset = Random.Range(0f, 10f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float swayX = Mathf.Sin((Time.time + randomOffset) * swaySpeed) * swayAmountX;
+        float swayZ = Mathf.Sin((Time.time + randomOffset) * swaySpeed) * swayAmountZ;
+
+        transform.localRotation = startRot * Quaternion.Euler(swayX, 0f, swayZ);
     }
 }
